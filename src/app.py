5a9c3429f65294ee570caac40345fad2b91fbd12
Jonathan Thorne <jonathanthorne13@gmail.com>
2020-04-21 11:28:19 -0400

    added some info for debugging purposes

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
 from appdirs import *
 import os, webbrowser, utilities.xml
-
+appname = "unquenched-editor"
+appauthor = "Unquenched Servant"
 if __name__ == "__main__":
     if sys.argv[1] == "-s":
         utilities.xml.split(sys.argv[2])
     elif sys.argv[1] == "-c":
         utilities.xml.change(sys.argv[2])
+    elif sys.argv[1] == "-p":
+        print(user_data_dir(appname, appauthor))
